df2b17e0a554091ca6e9c54717707866f249d1e4
clichetouche <twigg@jetspace.co>
2017-06-29 01:34:46 +0100

    Randomises list order

--- js/master.js
+++ js/master.js
@@ -198,3 +198,8 @@ function sliderUpdate() {
   console.log(hue);
 }
 sliderUpdate()
+
+var ul = document.querySelector('ul');
+for (var i = ul.children.length; i >= 0; i--) {
+    ul.appendChild(ul.children[Math.random() * i | 0]);
+}
